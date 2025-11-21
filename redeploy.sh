411144827dece32638f19b345ffdb4957cc911ca
spongehah <2635879218@qq.com>
2025-11-21 09:55:04 +0800

    fix: improve docker-compose

--- redeploy.sh
+++ redeploy.sh
@@ -1,4 +1,16 @@
-docker compose down && \
-docker compose down --rmi local && \
+#!/bin/bash
+
+# 停止并删除容器
+echo "停止容器..."
+docker compose down
+
+# 删除本地镜像
+echo "删除本地镜像..."
+docker compose down --rmi local
+
+# 启动服务
+echo "启动服务..."
 docker compose up -d
+
+echo "重新部署完成！"
 
